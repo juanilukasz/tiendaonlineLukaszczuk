ac63454fda8ed2d33ed0384000cdfe6f2dd7d7a9
juanilukasz <72827873+juanilukasz@users.noreply.github.com>
2021-03-29 22:44:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+
 import './App.css';
+import React, {Fragment} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return(<div><div>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Sobre nosotras</a>
+      <a class="nav-item nav-link" href="#">Tienda online</a>
+      <a class="nav-item nav-link" href="#">Galeria</a>
     </div>
-  );
-}
-
-export default App;
+  </div>
+</nav>
+    </div>
+    
+    </div>
+  <div> <h1 align="center"> Jungla! </h1> <br></br>
+    <h2 align="center">Tienda de Plantas online</h2> </div>
+    </div>);
+  };
+  export default App
